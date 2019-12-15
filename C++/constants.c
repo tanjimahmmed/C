76a1613e3a498182cb4d61036a9dd83cdb39273b
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-12-16 00:07:06 +0600

    Add files via upload

---
+++ C++/constants.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+        // const float PI = 3.1432f;
+        // printf("%f\n", PI);
+    const char PI = 't';
+    const char PY = 'a';
+    const char LL = 'n';
+    const char LY = 'J';
+    const char JJ = 'i';
+    const char MM = 'm';
+
+   printf("%c %c %c %c %c %c\n", PI, PY, LL, LY, JJ, MM, PI+PY+LL+LY+JJ+MM);
+    return 0;
+}
